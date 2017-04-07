f40220adc26f598e80e1eb6d5e8aa58ad01b3447
Vincent Eberle <veberle162@gmail.com>
2017-04-07 02:11:45 -0400

    Merge branch 'master' of https://github.com/ContextualCamouflage/contextual-camoflage-frontend

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
+import config from "../config/environment";
 
-export default DS.JSONAPIAdapter.extend({
-  namespace: 'api'
+export default DS.RESTAdapter.extend({
+  host: config.API_HOST,
+  namespace: "api/v1"
 });
--- config/environment.js
+++ config/environment.js
@@ -24,6 +24,7 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
+      ENV.API_HOST = "https://con-camo-api.herokuapp.com"
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
@@ -35,6 +36,7 @@ module.exports = function(environment) {
     // Testem prefers this...
     ENV.locationType = 'none';
 
+
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
@@ -43,6 +45,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.API_HOST = "https://con-camo-api.herokuapp.com"
 
   }
 

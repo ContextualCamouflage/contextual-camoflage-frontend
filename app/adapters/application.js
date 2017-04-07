045bb7226d756d833251e06e12249d0b1e720235
danigirl329 <danigreaves@gmail.com>
2017-04-07 01:21:25 -0400

    Passing host name through environment variables

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
+import config from "../config/environment";
 
 export default DS.RESTAdapter.extend({
-  host: "https://con-camo-api.herokuapp.com",
+  host: config.API_HOST,
   namespace: "api/v1"
 });
